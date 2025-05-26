49ff7e05b685343370fff43e60c2be415dece00c
Noor Basowaidan <93840370+noor-faozi@users.noreply.github.com>
2025-05-26 17:39:29 +0400

    feat: add admin bottom navbar

--- lib/layout/admin_layout.dart
+++ lib/layout/admin_layout.dart
@@ -1,3 +1,8 @@
+import 'package:albaderapp/screens/admin/attendance.dart';
+import 'package:albaderapp/screens/admin/employees.dart';
+import 'package:albaderapp/screens/admin/holidays.dart';
+import 'package:albaderapp/screens/admin/overtime_attendance.dart';
+import 'package:albaderapp/screens/admin/reports.dart';
 import 'package:flutter/material.dart';
 
 class AdminLayout extends StatefulWidget {
@@ -11,11 +16,35 @@ class _AdminLayoutState extends State<AdminLayout> {
   int _selectedIndex = 0;
 
   final List<Widget> _screens = [
-
+    const Employees(),
+    const Attendance(),
+    const OvertimeAttendance(),
+    const Holidays(),
+    const Reports()
   ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.groups), label: 'Employees'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.punch_clock), label: 'Attendance'),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Users'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.more_time), label: 'Overtime'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.event), label: 'Holiday'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.analytics), label: 'Reports'),
+        ],
+      ),
+    );
   }
 }
