980294f56eab0f2a70a81c7273d5a5df481680ca
Noor Basowaidan <93840370+noor-faozi@users.noreply.github.com>
2025-06-22 20:06:34 +0400

    feat: add Supabase import and initial state management for AddHolidayScreen

--- lib/screens/add_holiday_screen.dart
+++ lib/screens/add_holiday_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AddHolidayScreen extends StatefulWidget {
   const AddHolidayScreen({super.key});
@@ -8,6 +9,24 @@ class AddHolidayScreen extends StatefulWidget {
 }
 
 class _AddHolidayScreenState extends State<AddHolidayScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final  _descriptionController = TextEditingController(); 
+  DateTime selectedDate = DateTime.now();
+  final String? createdBy = Supabase.instance.client.auth.currentUser?.id;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
