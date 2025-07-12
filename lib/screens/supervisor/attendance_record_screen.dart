06a840767365b614508cffe81776e4a0268e0844
Noor Basowaidan <93840370+noor-faozi@users.noreply.github.com>
2025-07-12 09:23:36 +0400

    refactor: add supervisor folder and files

---
+++ lib/screens/supervisor/attendance_record_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AttendanceRecordScreen extends StatefulWidget {
+  const AttendanceRecordScreen({super.key});
+
+  @override
+  State<AttendanceRecordScreen> createState() => _AttendanceRecordScreenState();
+}
+
+class _AttendanceRecordScreenState extends State<AttendanceRecordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
